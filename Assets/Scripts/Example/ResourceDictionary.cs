cddd6da49cec737a8a19932bf8857f62f69576b7
Nikolai Rodin <mashyrinwork@gmail.com>
2020-12-03 14:25:59 +0300

    Init

---
+++ Assets/Scripts/Example/ResourceDictionary.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using DictionarySerialization.Source;
+
+namespace DictionarySerialization.Example
+{
+    [Serializable]
+    public class ResourceDictionary
+        : SerializableDictionary<int, GameObject>
+    {
+    }
+}
---
+++ Assets/Scripts/Example/ResourceDictionarySO.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DictionarySerialization.Example
+{
+    [CreateAssetMenu(
+        fileName = "ResourceDictionary",
+        menuName = "ScriptableObjects/ResourceDictionary",
+        order = 1)]
+    public class ResourceDictionarySO : ScriptableObject
+    {
+        [SerializeField]
+        private ResourceDictionary _resources;
+    }
+}
